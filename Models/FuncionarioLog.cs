968948a04825d06b8175e9ea25a7ebafef83f86c
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-19 15:15:27 -0300

    Construtor do FuncionarilLog

--- Models/FuncionarioLog.cs
+++ Models/FuncionarioLog.cs
@@ -1,12 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
 
 namespace pasta_projeto.Models
 {
     public class FuncionarioLog : Funcionario
     {
-        
+        public FuncionarioLog() { }
+
+        public FuncionarioLog(Funcionario funcionario, TipoAcao tipoAcao, string partitionKey, string rowKey)
+        {
+            base.Id = funcionario.Id;
+            base.Nome = funcionario.Nome;
+            base.Endereco = funcionario.Endereco;
+            base.Ramal = funcionario.Ramal;
+            base.EmailProfissional = funcionario.EmailProfissional;
+            base.Departamento = funcionario.Departamento;
+            base.Salario = funcionario.Salario;
+            base.DataAdmissao = funcionario.DataAdmissao;
+            TipoAcao = tipoAcao;
+            JSON = JsonSerializer.Serialize(funcionario);
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+        }
+
+        public TipoAcao TipoAcao { get; set; }
+        public string JSON { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
     }
 }

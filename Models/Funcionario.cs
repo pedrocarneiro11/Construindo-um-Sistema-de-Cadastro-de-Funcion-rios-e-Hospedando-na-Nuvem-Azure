47029505e809b537fb3a70eb6e013c3678511b9f
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-19 15:14:54 -0300

    Construtor da classe  Funcionario

--- Models/Funcionario.cs
+++ Models/Funcionario.cs
@@ -5,8 +5,30 @@ using System.Threading.Tasks;
 
 namespace pasta_projeto.Models
 {
+    
     public class Funcionario
     {
+        public Funcionario() { }
+
+        public Funcionario(int id, string nome, string endereco, string ramal, string emailProfissional, string departamento, decimal salario, DateTime dataAdmissao)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            Ramal = ramal;
+            EmailProfissional = emailProfissional;
+            Departamento = departamento;
+            Salario = salario;
+            DataAdmissao = dataAdmissao;
+        }
         
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Ramal { get; set; }
+        public string EmailProfissional { get; set; }
+        public string Departamento { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime DataAdmissao { get; set; }
     }
 }

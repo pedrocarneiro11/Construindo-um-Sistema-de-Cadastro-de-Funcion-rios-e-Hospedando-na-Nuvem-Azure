76f6e66f99dbe6af54f5ef5d9f34d3d91825bdbb
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-19 15:00:16 -0300

    commit inicial

---
+++ Models/Funcionario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pasta_projeto.Models
+{
+    public class Funcionario
+    {
+        
+    }
+}
